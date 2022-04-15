2da1077b04a3465efc7c5467b793d07a675eb052
gongh95 <gonzalo.simon.1995@gmail.com>
2022-04-15 16:30:25 -0300

    funciones principales

--- app.js
+++ app.js
@@ -6,12 +6,43 @@ const desencriptarBtn = document.getElementById("desencriptador");
 const imgCuandoInputVacio = document.getElementById("img-cuando-no-hay-input");
 // Declarar e inicializar variables
 let stringAEncriptar = "";
+let stringEncriptado = "";
 
 
+function prueba() {
+    stringAEncriptar = textoInicio.value;
+}
+function pruebaPasoDos() {
+    prueba();
+    textoInicio.value = ""; // Limpia textarea
+    console.log(stringAEncriptar.includes("a"));
+    console.log(stringAEncriptar.includes("e"));
+    console.log(stringAEncriptar.includes("i"));
+    console.log(stringAEncriptar.includes("o"));
+    console.log(stringAEncriptar.includes("u"));
+}
 
-function ingresarString(textoInicio) {
-    return textoInicio.includes("a","e","i","o","u");
-    textoInicio.value = "" // Limpia textarea
+function encriptar() {
+    switch (stringAEncriptar.includes()) {
+        case "a":
+            stringEncriptado = stringAEncriptar.replace("a", "ai");
+            console.log(stringEncriptado);
+            break;
+        case "e":
+            stringEncriptado = stringAEncriptar.replace("e", "enter");
+            break;
+        case "i":
+            stringEncriptado = stringAEncriptar.replace("i", "imes");
+            break;
+        case "o":
+            stringEncriptado = stringAEncriptar.replace("o", "ober");
+            break;
+        case "u":
+            stringEncriptado = stringAEncriptar.replace("u", "ufat");
+            break;
+        default:
+            break;
+    }
 }
 //function encriptar() {}
 
@@ -21,5 +52,5 @@ function alertarTextoInicio() {
     textoInicio.value = "" // Limpia textarea
 }
 
-encriptarBtn.onclick = alertarTextoInicio;
-desencriptarBtn.onclick = ingresarString(textoInicio.value);
+//encriptarBtn.onclick = alertarTextoInicio;
+encriptarBtn.onclick = pruebaPasoDos;
